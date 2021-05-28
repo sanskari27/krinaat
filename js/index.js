4fdc6b9ba5e05d4f8b74b689852c63d82091be49
Sanskar <sanskarkumar85111@gmail.com>
2021-05-29 03:07:26 +0530

    home-page completed

--- js/index.js
+++ js/index.js
@@ -30,3 +30,31 @@ $(".nav-item").mouseenter(function () {
         $(this).removeClass("dot");
     }
 });
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCp5QoHWnUTKdqp_gQSuCDjsB0YnzM5AXQ",
+    authDomain: "krinaat-web.firebaseapp.com",
+    projectId: "krinaat-web",
+    storageBucket: "krinaat-web.appspot.com",
+    messagingSenderId: "947589267202",
+    appId: "1:947589267202:web:c5256ed49ed46c5cf1cc1b",
+    measurementId: "G-L10E5L1EF2"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var firestore = firebase.firestore();
+
+$("#send-msg").click(function (e) { 
+    let email = $("#email").val();
+    let msg = $("#message").val();
+    $("#email").val("");
+    $("#message").val("");
+    firestore.collection("messages").add({
+        email_id : email,
+        message : msg 
+    });
+
+});
